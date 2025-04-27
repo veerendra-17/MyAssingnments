55b7f823451f3b1a212ddb12cab347822ba40486
veerendra-17 <veerug@outlook.com>
2025-04-27 10:42:15 +0530

    Add files via upload
    Day 1 Assignment for JAVA basics

---
+++ day1/Datatypes.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+public class Datatypes {
+
+	public static void main(String[] args)
+	{
+		byte byte_age = 29;
+		System.out.println("Age is "+byte_age);
+		int int_postcode =5331;
+		System.out.println(int_postcode);
+		long lng_phno =9009000050L;
+		System.out.println(lng_phno);
+		short sht_regno=8906;
+		System.out.println(sht_regno);
+		float ftl_speed= 98.889988f;
+		System.out.println(ftl_speed);
+		double dbl_speed= 99.77777488399;
+		System.out.println(dbl_speed);
+		char chr_name ='A';
+		System.out.println(chr_name);
+		boolean isVisible =true;
+		String str_name="Sample";
+		System.out.println(str_name);
+		
+		
+
+	}
+
+}
---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class HelloTestLeaf
+{
+	public static void main(String[] args) 
+	{
+	System.out.println("Hello TestLeaf class");	
+	System.out.println("Thank you for");
+	}
+
+}
